225c08e814b62a3dce073c497a324106f9fe6608
Matheus Bonnet <82242779+MatheusBonnet@users.noreply.github.com>
2021-09-10 20:31:41 -0300

    app js

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
+const listaTarefas = [];
+
+
+
+
+listaTarefas.push(tarefa1);
+
+renderLista();
+
+function fabricaTarefas() {
+    let descricao = document.getElementById('descricaoTarefa').value;
+    let prioridade = document.getElementById('prioridadeTarefa').value;
+    let tarefa = {
+        descricao: descricao,
+        status: "aberto",
+        prioridade: prioridade
+    };
+    listaTarefas.push(tarefa);
+    alert("Tarefa criada");
+    renderLista();
+}
+
+function renderLista() {
+    let divLista = document.getElementById('divLista');
+    let template = '';
+    for (let i = 0; i < listaTarefas.length; i++) {
+        template += `<div class="tarefa ${listaTarefas[i].prioridade}">
+                <p class="descTarefa"> ${listaTarefas[i].descricao}  </p>
+                <button>Excluir</button>
+                <input type="checkbox" name="" id="">
+                </div>`;     
+    }
+    divLista.innerHTML = template;
+}
 
